ad9171e5ff991a01161b6366249455ac15c39b2f
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-25 12:02:03 +0700

    add new function

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 let rectangleLength = prompt("How long is the length?")
 let rectangleWidth = prompt("How long is the width?")
 
-const rectangleArea = parseInt(rectangleWidth) * parseInt(rectangleLength) 
+const rectangleArea = parseInt(rectangleWidth) * parseInt(rectangleLength)
 
 console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+switch (true) {
+    case rectangleLength > rectangleWidth:
+        let sizeDifference = parseInt(rectangleLength) - parseInt(rectangleWidth)
+        console.log(`The length is longer by ${sizeDifference}`);
+        break
+
+    case rectangleLength < rectangleWidth:
+        let sizeDifference = parseInt(rectangleWidth) - parseInt(rectangleLength)
+        console.log(`The width is longer by  ${sizeDifference}`);
+        break
+    default:
+        console.log("no difference, your shape is a square!");
+        break
+
+}
 
 // Feel free to have some experiments with variety of shape formulas
