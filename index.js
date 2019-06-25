0ef1067e0ace95c8cc8f6eba53ba312b1a5754d3
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-25 11:43:16 +0700

    add function

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const rectangleWidth = 100
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+// Feel free to have some experiments with variety of shape formulas
